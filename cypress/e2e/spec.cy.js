a01731a65f00a5068207b35bd33e362c85ed3cd0
Abdul Rehman <arhere.ar@gmail.com>
2025-08-06 16:40:05 +0500

    commit "specs created"

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,7 @@
+describe('My First Test', () => {
+  it('visits example page', () => {
+    cy.visit('https://example.cypress.io');
+    cy.contains('type').click();
+    cy.url().should('include', '/commands/actions');
+  });
+});
